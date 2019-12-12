75e9cd739b4130ac2108219814035d6684610e13
Ricardo Bautista <rickyhaswifi@gmail.com>
2019-12-12 10:05:27 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,47 @@
+document.getElementById('takedownForm').addEventListener('submit', function(event){
+  event.preventDefault();
+  });
+  
+  document.getElementById('nameInput').addEventListener('input', function(e){
+    let labelname = e.target.value;
+    document.getElementById('nameOutput').innerHTML = labelname;
+  });
+  document.getElementById('clientSongInput').addEventListener('input', function(e){
+    let clientsong = e.target.value;
+    document.getElementById('clientSongOutput').innerHTML = clientsong;
+  });
+  
+  
+  document.getElementById('songInput').addEventListener('input', function(e){
+    let songinfr = e.target.value;
+    document.getElementById('songOutput').innerHTML = songinfr;
+  });
+  
+  document.getElementById('artistInput').addEventListener('input', function(e){
+    let artistinfr = e.target.value;
+    document.getElementById('artistOutput').innerHTML = artistinfr;
+  });
+  
+  document.getElementById('ytInput').addEventListener('input', function(e){
+    let ytartist = e.target.value;
+    document.getElementById('ytOutput').innerHTML = ytartist;
+  });
+  
+  
+  songs = [];
+  
+  const resetForm = () => {
+    document.getElementById('takedownForm').reset();
+    document.getElementById('nameOutput').innerHTML = '';
+    document.getElementById('clientSongOutput').innerHTML = '';
+    document.getElementById('songOutput').innerHTML = '';
+    document.getElementById('artistOutput').innerHTML = '';
+    document.getElementById('ytOutput').innerHTML = '';
+  }
+  
+  const copyPasta = () => {
+  let copyText = document.getElementById('copyCont');
+    copyText.select();
+    copyText.setSelectionRange(0, 99999)
+    document.execCommand("copy");
+  }
